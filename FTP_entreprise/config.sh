6ed2b1ee09fba683b169f6aaf18535649e690841
Yassine Berrada <160861264+YassineBerrada@users.noreply.github.com>
2024-12-13 13:40:29 +0100

    Update config.sh

--- FTP_entreprise/config.sh
+++ FTP_entreprise/config.sh
@@ -1,1 +1,10 @@
+#!/bin/bash
 
+# Activer l'interface réseau
+ip link set dev eth0 up
+
+# Configurer l'adresse IP et le masque de sous-réseau
+ip addr add 120.0.82.5/23 dev eth0
+
+# Configurer la passerelle par défaut
+#ip route add default via 120.0.85.131/25
