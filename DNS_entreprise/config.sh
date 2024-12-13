c0a5d4ae0c270ca8a3ef44e4eed100664db5e7a8
yasmina-ab-12 <yasminaabouelabbas@gmail.com>
2024-12-13 11:02:56 +0100

    Update config.sh

--- DNS_entreprise/config.sh
+++ DNS_entreprise/config.sh
@@ -1,1 +1,10 @@
+#!/bin/bash
 
+# Activer l'interface réseau
+ip link set dev eth0 up
+ip addr add 120.0.82.2/23 dev eth0
+
+# Ajouter la route par défaut
+ip route add default via 120.0.82.7
+
+service bind9 start
