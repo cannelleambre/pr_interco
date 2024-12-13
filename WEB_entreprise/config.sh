d7b6d0a10a292c4a10bdfb902d9287506c0e0ac0
yasmina-ab-12 <yasminaabouelabbas@gmail.com>
2024-12-13 12:02:36 +0100

    Update config.sh

--- WEB_entreprise/config.sh
+++ WEB_entreprise/config.sh
@@ -1,1 +1,12 @@
+#!/bin/bash
+
+# Activer l'interface réseau
+ip link set dev eth0 up
+ip addr add 120.0.82.4/23 dev eth0
+
+# Ajouter la route par défaut
+ip route add default via 120.0.82.7
+
+# Démarrer le serveur web Apache
+httpd -D FOREGROUND
 
