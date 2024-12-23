a76ab21a6688fcf6fd1367a081d0c752fb9e97cc
iammathisrgd <mathisrigaud5@gmail.com>
2024-12-23 13:32:10 +0100

    Ajout config backbone

---
+++ Backbone/routeur_backbone/script.sh
@@ -0,0 +1,4 @@
+ip route del default via 120.0.85.129
+ip route add default via 120.0.85.130
+
+sleep infinity
---
+++ Backbone/routeur_web_dns_secondaire/script.sh
@@ -0,0 +1,4 @@
+ip route del default via 120.0.84.129
+ip route add default via 120.0.84.130
+
+sleep infinity
