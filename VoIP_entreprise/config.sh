4c492df1f47a934b363899d452566a18b07daa94
Yassine BERRADA <yassine.berrada2020@gmail.com>
2025-01-11 18:26:55 +0100

    Mise à jour de VoIP_entreprise et docker-compose.yml

--- VoIP_entreprise/config.sh
+++ VoIP_entreprise/config.sh
@@ -1,1 +1,15 @@
+#!/bin/bash
+
+# Met à jour les paquets et installe Asterisk
+apt-get update
+apt-get install -y asterisk
+
+# Copie les fichiers de configuration nécessaires (ajoutez-les dans votre conteneur)
+cp /etc/asterisk/sip.conf /etc/asterisk/sip.conf.bak
+cp /config_files/sip.conf /etc/asterisk/sip.conf
+
+# Redémarre le service Asterisk
+service asterisk restart
+
+echo "Configuration du serveur VoIP terminée."
 
